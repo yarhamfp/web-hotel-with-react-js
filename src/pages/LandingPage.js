277396e02e3f799d58bcbcaf72ff74dea104b807
yarhamfp <yarhamfp@gmail.com>
2020-05-04 13:13:50 +0700

    create hero component and add import icon

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import Header from "parts/Header";
+import landingPage from "json/LandingPage.json";
+import Hero from "parts/Hero";
 export default class LandingPage extends Component {
   render() {
     return (
       <>
         <Header {...this.props}></Header>
+        <Hero data={landingPage.hero} />
       </>
     );
   }
---
+++ src/parts/Hero.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ImageHero from "assets/images/img-hero.jpg";
+import ImageHero_ from "assets/images/img-hero-frame.jpg";
+import IconCities from "assets/images/icons/ic_cities.svg";
+import IconTraveler from "assets/images/icons/ic_traveler.svg";
+import IconTreasure from "assets/images/icons/ic_treasure.svg";
+
+export default function Hero(props) {
+  return <div></div>;
+}
