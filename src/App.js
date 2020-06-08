98927b57971f2ffb4763c2bf7bb75c1b5de4a3ed
yarhamfp <yarhamfp@gmail.com>
2020-06-08 21:03:10 +0700

    Create input number and input date component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "assets/scss/style.scss";
 import LandingPage from "pages/LandingPage";
+import Example from "pages/Example";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}></Route>
+        <Route exact path="/" component={LandingPage}></Route>
+        <Route exact path="/example" component={Example}></Route>
       </Router>
     </div>
   );
