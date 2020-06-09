5771ca7ab5794acd105051668efffd3ace7df5e4
yarhamfp <yarhamfp@gmail.com>
2020-06-09 14:18:21 +0700

    Create Detail Page Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "assets/scss/style.scss";
 import LandingPage from "pages/LandingPage";
+import DetailsPage from "pages/DetailsPage";
 import Example from "pages/Example";
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <div className="App">
       <Router>
         <Route exact path="/" component={LandingPage}></Route>
-        <Route exact path="/example" component={Example}></Route>
+        <Route exact path="/properties/:id" component={DetailsPage}></Route>
+        <Route path="/example" component={Example}></Route>
       </Router>
     </div>
   );
